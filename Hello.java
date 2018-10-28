d1c2aa2854cd12d88825ccbab52fb623cd01f4e6
sayole <sylee8191@gmail.com>
2018-10-28 18:02:00 +0900

    Second Commit

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
       System.out.println("after change First Commit");
-      System.out.println("vovovovovovovovovovovovov");
+
    }
 }
