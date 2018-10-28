5dcb4998de03a33822dff67f60518060b5fdc6fa
sayole <sylee8191@gmail.com>
2018-10-28 18:00:27 +0900

    First Commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+// Hello.java
+public class Hello {
+   public static void main(String[] args) {
+      System.out.println("Hello, world from GIT!");
+      System.out.println("after change First Commit");
+      System.out.println("vovovovovovovovovovovovov");
+   }
+}
